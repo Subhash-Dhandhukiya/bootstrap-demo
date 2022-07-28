e8e0ecbf1e982d1f70e9e0e74d0aa71bb1d4295c
Subhash-Dhandhukiya <subhashdhandhukiya69@gmail.com>
2022-07-28 19:16:50 +0530

    Handle Nav Menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Company from "./pages/Company";
+import Contact from "./pages/Contact";
+import FAQ from "./pages/FAQ";
+import Order from "./pages/Order";
 
 const App = () => {
-  return <Navbar />;
+  //   let component;
+
+  //   switch (window?.location?.pathname) {
+  //     case "/":
+  //       component = <Home />;
+  //       break;
+  //     case "/company":
+  //       component = <Company />;
+  //       break;
+  //     case "/contact":
+  //       component = <Contact />;
+  //       break;
+  //     case "/faq":
+  //       component = <FAQ />;
+  //       break;
+  //     case "/order":
+  //       component = <Order />;
+  //       break;
+  //   }
+
+  return (
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route to="/" element={<Home />} />
+          <Route to="/company" element={<Company />} />
+          <Route to="/contact" element={<Contact />} />
+          <Route to="/faq" element={<FAQ />} />
+          <Route to="/order" element={<Order />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,31 @@
 import React from "react";
 import Logo from "../assets/Logo.svg";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
-//35b8be
+const CustomLink = ({ href, children, ...props }) => {
+  const path = window.location.pathname;
+  // const resolvedPath = useResolvedPath(href);
+  // const isActive = useMatch({path:resolvedPath.pathname , end:true})
+  return (
+    <li class="nav-item px-2">
+      <a
+        {...props}
+        class={path === href ? "nav-link active-color" : "nav-link"}
+        href={href}
+      >
+        {children}
+      </a>
+    </li>
+  );
+};
+
 const Navbar = () => {
   return (
     <div>
-      <div className="color-primary p-3 text-white text-center">
-        <p>We're open and available for takeaway & delivery. Order Now</p>
+      <div className="color-primary p-2 text-white text-center">
+        <p className="mt-2">
+          We're open and available for takeaway & delivery. Order Now
+        </p>
       </div>
 
       <nav className="navbar navbar-expand-sm bg-white container">
@@ -25,31 +44,11 @@ const Navbar = () => {
         </div>
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
           <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Order
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Company
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                FAQ
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Contact
-              </a>
-            </li>
+            <CustomLink href="/">Home</CustomLink>
+            <CustomLink href="/order">Order</CustomLink>
+            <CustomLink href="/company">Company</CustomLink>
+            <CustomLink href="/faq">FAQ</CustomLink>
+            <CustomLink href="contact">Contact</CustomLink>
           </ul>
         </div>
       </nav>
