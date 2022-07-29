2e289a82a10d0cee6f887594ef940c7e02548053
Subhash-Dhandhukiya <subhashdhandhukiya69@gmail.com>
2022-07-29 19:12:12 +0530

    First Page Complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,8 @@ import FoodCard from "../components/FoodCard";
 import WorkCard from "../components/WorkCard";
 import "../styles/home.css";
 import { images } from "../utils/images";
+import Logo from "../assets/Logo.svg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -191,6 +193,134 @@ const Home = () => {
                 title={"See our FAQ"}
                 onClick={() => alert("Coming soon...")}
               />
+            </div>
+          </div>
+        </div>
+
+        <div className="row mt-5 pt-5 background-color">
+          <div className="col-md-6 p-5">
+            <div className="mt-5">
+              <h1
+                className="display-4 active-color"
+                style={{ fontWeight: "bolder" }}
+              >
+                Call our store and takeaway when it suits you best.
+              </h1>
+              <p className="mt-4 light-gray description">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500.
+              </p>
+            </div>
+
+            <div className="mt-5">
+              <CustomButton
+                title={"Ph. +61 233 2333"}
+                onClick={() => alert("Coming soon...")}
+              />
+            </div>
+          </div>
+          <div className="col-md-6 p-5">
+            <img
+              src={images.img4}
+              alt="side_image"
+              class="img-fluid image-container"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid background-color ">
+        <div className="container mt-5 p-4 ">
+          <div className="row">
+            <div className="mt-5 col-md-5">
+              <img src={Logo} alt="logo" className="img-fluid logo" />
+
+              <p className="mt-3 light-gray">Takeaway & Delivery template</p>
+              <p className="light-gray">for small - medium businesses. </p>
+            </div>
+
+            <div className="mt-5 col-md-7 row">
+              <div className="d-flex">
+                <div className="mx-5">
+                  <p className="text-dark">COMPANY</p>
+                  <ul className="nav flex-column">
+                    <li className="nav-item">
+                      <Link className="nav-link light-gray" to="/">
+                        Home
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link light-gray" to="/order">
+                        Order
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link light-gray" to="/faq">
+                        FAQ
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link light-gray" to="/contact">
+                        Contact
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="mx-5">
+                  <p className="text-dark">TEMPLATE</p>
+                  <ul className="nav flex-column">
+                    <li className="nav-item">
+                      <Link className="nav-link light-gray" to="/">
+                        Style Guide
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link light-gray" to="/">
+                        Changelog
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link light-gray" to="/">
+                        Licence
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link light-gray" to="/">
+                        Webflow University
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="mx-5">
+                  <p className="text-dark">FLOWBASE </p>
+                  <ul className="nav flex-column">
+                    <li className="nav-item">
+                      <Link className="nav-link light-gray" to="/">
+                        More Cloneables
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr />
+
+          <div className="d-flex align-items-center justify-content-between mt-5">
+            <p className="light-gray">Built by Flowbase · Powered by Webflow</p>
+            <div className="d-flex">
+              <a className="nav-link light-gray ps-5" href="">
+                Facebook
+              </a>
+              <a className="nav-link light-gray ps-5" href="">
+                Instagram
+              </a>
+              <a className="nav-link light-gray ps-5" href="">
+                Youtube
+              </a>
             </div>
           </div>
         </div>
